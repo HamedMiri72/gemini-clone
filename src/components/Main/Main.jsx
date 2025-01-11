150dbdcede17ef9a6c092b43211941697e00914e
HamedMiri72 <hamedmiri72@gmail.com>
2025-01-11 14:47:28 +0000

    ui desgin

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -40,6 +40,21 @@ const Main = () => {
 
             </div>
 
+            <div className="main-bottom">
+                <div className="search-box">
+                    <input type="text" placeholder='Enter a prompt here'/>
+                    <div>
+                        <img src={assets.gallery_icon} alt="" />
+                        <img src={assets.mic_icon} alt="" />
+                        <img src={assets.send_icon} alt="" />
+                    </div>
+                </div>
+
+                <p className="bottom-info">
+                    this Ai may inaccurate info, inclding about people, so double check its responses. your privacy and Hamed's App.
+                </p>
+            </div>
+
 
         </div>
         
